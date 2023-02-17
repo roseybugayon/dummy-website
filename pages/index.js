3de1ee56104b5333d178197697823b19debda765
roseybugayon <rbugayon@ucsd.edu>
2023-02-16 17:36:58 -0800

    feat: added flask to project

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,33 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import { useState } from "react";
+import axios from "axios";
 
 export default function Home() {
+  const [profileData, setProfileData] = useState(null);
+
+  function getData() {
+    axios({
+      method: "GET",
+      url: "http://127.0.0.1:5000/profile",
+    })
+      .then((response) => {
+        const res = response.data;
+        setProfileData({
+          profile_name: res.name,
+          about_me: res.about,
+        });
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+      });
+  }
+
   return (
     <div className="container">
       <Head>
@@ -12,12 +37,18 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        Get started by editing <code>pages/index.js</code>
+        <p>To get your profile details: </p>
+        <button onClick={getData}>Click me</button>
+        {profileData && (
+          <div>
+            <p>Profile name: {profileData.profile_name}</p>
+            <p>About me: {profileData.about_me}</p>
+          </div>
+        )}
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
